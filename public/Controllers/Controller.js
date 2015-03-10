f68d52ee6f32dbc29fcbe152b898151352f78339
Paulo Diniz <pdiniz_100@yahoo.com>
2015-03-10 11:34:50 -0700

    Small improvements have been made and project is now complete

--- public/Controllers/Controller.js
+++ public/Controllers/Controller.js
@@ -3,7 +3,7 @@
  */
 angular.module('sql.controller', [])
 
-  .controller('SqlController', ['$scope', 'Tables' , function($scope, Tables) {
+  .controller('SqlController', ['$scope', 'Tables', function($scope, Tables) {
     $scope.name = '';
     $scope.columns = [{id: 'col1'}];
     $scope.tables = Tables.tables;
@@ -12,43 +12,49 @@ angular.module('sql.controller', [])
     $scope.addNewCol = function() {
       var newItemNo = $scope.columns.length + 1;
       $scope.columns.push({'id': 'col' + newItemNo});
-    };    $scope.removeLastCol = Tables.removeLastCol;
+    };
+    $scope.removeLastCol = Tables.removeLastCol;
     $scope.removeLastCol = function() {
       $scope.columns.pop();
     };
-    $scope.addNewTable = function(){
+    $scope.addNewTable = function() {
       Tables.addNewTable($scope.name, $scope.columns, $scope.contains, $scope.depends);
       $scope.name = '';
       $scope.columns = [{id: 'col1'}];
       $scope.contains = null;
       $scope.depends = null;
       $scope.tables = Tables.tables;
-      $scope.tableName1={};
-      $scope.tableName={};
+      $scope.tableName1 = {};
+      $scope.tableName = {};
     };
-    $scope.updateDependency = function(){
-      $scope.depends= $scope.tableName1.name;
+    $scope.updateDependency = function() {
+      $scope.depends = $scope.tableName1.name;
     };
-    $scope.updateContains = function(){
+    $scope.updateContains = function() {
       $scope.contains = $scope.tableName.name;
     };
   }])
   .factory('Tables', function() {
     var tables = [];
     var addNewTable = function(name, columns, contains, depends) {
-      columns.push({name: name+"Id"});
-      if (contains === null && depends !== null){
-        columns.push({name: depends+'Id'+'(FK)'})
+      columns.push({name: name + "Id"});
+      if (contains === null && depends !== null) {
+        columns.push({name: depends + 'Id' + '(FK)'})
       }
-      else if (contains !== null && depends === null){
-        for (var x= 0, count = tables.length; x < count; x++){
-          if (depends === tables[x].name){
-            tables[x].columns.push({name: contains + "Id"+'(FK)'});
+      else if (contains !== null && depends === null) {
+        for (var x = 0, count = tables.length; x < count; x++) {
+          if (depends === tables[x].name) {
+            tables[x].columns.push({name: contains + "Id" + '(FK)'});
           }
         }
       }
-      else if (contains !== null && depends !== null){
-       tables.push({name: contains+name, columns:[{name: contains+name+"Id"}, {name: contains + "Id"+'(FK)'}, {name: name + "Id"+'(FK)'}]})
+      else if (contains !== null && depends !== null) {
+        if (contains === depends) {
+          tables.push({
+            name: contains + name,
+            columns: [{name: contains + name + "Id"}, {name: contains + "Id" + '(FK)'}, {name: name + "Id" + '(FK)'}]
+          })
+        }
       }
 
       tables.push({name: name, columns: columns});
